b3dc559189e8bb0aeefdf19c9b78a3d97d5e3fed
Mauricio <86811014+MAURO5453aa@users.noreply.github.com>
2025-02-27 15:16:48 -0500

    Empezamos a crear el crud con Crear cliente

---
+++ Backend/src/routes/customerRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Customer = require('../models/Customer');//importamos el modelo de la base de datos
+const router = express.Router();
+//ruta para obtener todos los clientes
+
+// crear un nuevo cliente
+
+router.post("/", async (req, res) => {
+
+    try{
+        const { username, email, password , role }=req.body;//obtenemos los datos del cliente
+        const newCustomer = new Customer({ username, email, password , role });//creamos un nuevo cliente
+        await newCustomer.save();//guardamos el cliente en la base de datos
+        res.send("Cliente creado correctamente");//mensaje de que el cliente se creo correctamente
+        res.status(201).json(newCustomer);//
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+
+});
