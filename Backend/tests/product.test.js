ac3bd54f07875169719a75aa4ba0bc5410505363
Mauricio <86811014+MAURO5453aa@users.noreply.github.com>
2025-03-10 20:48:22 -0500

    Prueba del pipeline en github

--- Backend/tests/product.test.js
+++ Backend/tests/product.test.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = require(path.resolve(__dirname, "../src/index"));
 
 
-describe("Pruebas de la API de products", () => {
+describe("Pruebas de la API de  products", () => {
   test("Debe devolver un array de productos", async () => {
     const response = await request(app).get("/products");
     expect(response.statusCode).toBe(200);
