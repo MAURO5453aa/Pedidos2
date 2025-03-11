1e7b93919767bc12de0b3857707c8e0ad1160b2d
Mauricio <86811014+MAURO5453aa@users.noreply.github.com>
2025-03-10 20:25:25 -0500

    modificacion para integrar pruebas a pipeline

--- Backend/tests/order.test.js
+++ Backend/tests/order.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../src/index"); // Importamos la app
 
-describe("Pruebas de la API de orders ", () => {
+describe("Pruebas de la API de orders", () => {
   test("Debe devolver un array de clientes", async () => {
     const response = await request(app).get("/orders");
     expect(response.statusCode).toBe(200);
